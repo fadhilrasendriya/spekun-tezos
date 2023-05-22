211d719c0857fc5bd03c9aee1179d918f13214ab
Rafi Dirgantara <rafi.dirgan@gmail.com>
2023-05-22 20:05:22 +0700

    add function to interract with smartcontract

--- scripts/interact.js
+++ scripts/interact.js
@@ -6,14 +6,50 @@ dotenv.config();
 
 const { RPC_URL, PRIVATE_KEY, CONTRACT_ADDRESS } = process.env;
 
-const add_spekun = async () => {
+async function add_spekun(id_sepeda, node) {
   const signer = await InMemorySigner.fromSecretKey(PRIVATE_KEY);
   const Tezos = new TezosToolkit(RPC_URL);
   Tezos.setProvider({ signer: signer });
 
   const contract = await Tezos.contract.at(CONTRACT_ADDRESS);
 
-  const tx = await contract.methods.add_sepeda(id_sepeda = 'x123').send();
+  const tx = await contract.methods.add_sepeda(id_sepeda = id_sepeda, node = node).send();
+
+  console.log(await tx.confirmation(1));
+}
+
+async function add_spekun(id_sepeda, node) {
+  const signer = await InMemorySigner.fromSecretKey(PRIVATE_KEY);
+  const Tezos = new TezosToolkit(RPC_URL);
+  Tezos.setProvider({ signer: signer });
+
+  const contract = await Tezos.contract.at(CONTRACT_ADDRESS);
+
+  const tx = await contract.methods.add_sepeda(id_sepeda = id_sepeda, node = node).send();
+
+  console.log(await tx.confirmation(1));
+}
+
+async function pinjam_spekun(id_sepeda, peminjam) {
+  const signer = await InMemorySigner.fromSecretKey(PRIVATE_KEY);
+  const Tezos = new TezosToolkit(RPC_URL);
+  Tezos.setProvider({ signer: signer });
+
+  const contract = await Tezos.contract.at(CONTRACT_ADDRESS);
+
+  const tx = await contract.methods.borrow_sepeda(id_sepeda = id_sepeda, peminjam = peminjam).send();
+
+  console.log(await tx.confirmation(1));
+}
+
+async function kembalikan_spekun(id_sepeda, peminjam, node) {
+  const signer = await InMemorySigner.fromSecretKey(PRIVATE_KEY);
+  const Tezos = new TezosToolkit(RPC_URL);
+  Tezos.setProvider({ signer: signer });
+
+  const contract = await Tezos.contract.at(CONTRACT_ADDRESS);
+
+  const tx = await contract.methods.return_sepeda(id_sepeda = id_sepeda, peminjam = peminjam, node = node).send();
 
   console.log(await tx.confirmation(1));
 }
